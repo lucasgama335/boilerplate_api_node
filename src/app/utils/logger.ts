aac2429448aeb549f7475416f2623055eeb844a2
Lucas Gama <lucasgama4545@gmail.com>
2026-07-24 12:40:48 -0300

    chore: make pino insert logs on local files too

--- src/app/utils/logger.ts
+++ src/app/utils/logger.ts
@@ -4,5 +4,24 @@ export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
     // Em desenvolvimento, usa o pino-pretty para ficar legível.
     // Em produção (NODE_ENV=production), cospe JSON puro (mais rápido e ideal para AWS/Datadog).
-    transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty', options: { colorize: true } } : undefined,
+    transport: {
+        // 'targets' permite enviar o log para vários lugares ao mesmo tempo
+        targets: [
+            // 1. Manda pro Console (com pino-pretty pra ficar legível)
+            {
+                target: 'pino-pretty',
+                options: { colorize: true },
+                level: 'info',
+            },
+            // 2. Manda para um Arquivo de texto
+            {
+                target: 'pino/file',
+                options: {
+                    destination: './logs/error.log', // Onde o arquivo será salvo
+                    mkdir: true, // Cria a pasta 'logs' automaticamente se não existir
+                },
+                level: 'error', // Dica: Só mande 'error' para o arquivo, assim seu disco não enche de logs de 'info'
+            },
+        ],
+    },
 });
